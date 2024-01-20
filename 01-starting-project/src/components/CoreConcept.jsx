22d96e200205f32986b3466d72f0c8e8882714bc
ksy <skuksyzkzk@gmail.com>
2024-01-20 21:52:08 +0900

    Feat: React first study

---
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,11 @@
+function CoreConcenpt(props) {
+    return (
+      <li>
+        <img src={props.image} alt={props.title}></img>
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+    )
+  }
+
+  export default CoreConcenpt;
